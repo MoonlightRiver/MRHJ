2de2e7a6feeabcb6e98d7854565ea2c5e85b1ed5
kjh618 <kimjihoon6188@daum.net>
2018-11-03 21:59:54 +0900

    Add time text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public GameObject enemyPrefab;
     public GameObject player;
+    public Text timeText;
 
     private Rigidbody2D playerRb2d;
     private Vector2 playerPosition;
+    private float secondsElapsed;
     
     void Start() {
         playerRb2d = player.GetComponent<Rigidbody2D>();
         StartCoroutine(SpawnEnemy());
+        secondsElapsed = 0;
     }
     
     void Update() {
         playerPosition = playerRb2d.position;
+        secondsElapsed += Time.deltaTime;
+        timeText.text = string.Format("{0:d2}:{1:d2}", (int)secondsElapsed / 60, (int)secondsElapsed % 60);
     }
 
     private IEnumerator SpawnEnemy() {
