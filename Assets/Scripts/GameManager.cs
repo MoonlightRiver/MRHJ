ed0e8464f366807b806d43eb3003b389eef16dc9
kjh618 <kimjihoon6188@daum.net>
2018-11-03 15:14:06 +0900

    Add enemy spawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public GameObject player;
+
+    private Rigidbody2D playerRb2d;
+    private Vector2 playerPosition;
+	
+	void Start() {
+        playerRb2d = player.GetComponent<Rigidbody2D>();
+        StartCoroutine(SpawnEnemy());
+	}
+	
+	void Update() {
+        playerPosition = playerRb2d.position;
+    }
+
+    private IEnumerator SpawnEnemy() {
+        while (true) {
+            float radius = Random.Range(2.5f, 5);
+            float angle = Random.Range(0, 360);
+            float x = radius * Mathf.Cos(angle);
+            float y = radius * Mathf.Sin(angle);
+            Vector2 startPosition = playerPosition + new Vector2(x, y);
+
+            Instantiate(enemyPrefab, startPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
