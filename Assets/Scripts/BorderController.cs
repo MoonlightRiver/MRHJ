052667b81a845f5ac4003aadd1113e4d88d5bf10
MoonlightRiver <baedro1@naver.com>
2018-11-25 15:36:01 +0900

    Now projectiles cannot penetrate unbreakable walls.

---
+++ Assets/Scripts/BorderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy Projectile")
+        {
+            Destroy(col.gameObject);
+        }
+        else if (col.gameObject.tag == "Player Projectile")
+        {
+
+            Destroy(col.gameObject);
+        }
+    }
+}
