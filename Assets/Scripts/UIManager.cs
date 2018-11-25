6d1f8550fbff1e0d2cca45b7251775a0d25a727e
kjh618 <kimjihoon6188@daum.net>
2018-11-26 00:50:50 +0900

    Add ability to toggle status panel using tab key

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject statusPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            statusPanel.SetActive(!statusPanel.activeSelf);
+        }
+    }
+}
