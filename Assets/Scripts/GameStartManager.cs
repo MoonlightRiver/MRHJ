098783385d58c836cdb2972b843e304b0487e916
kjh618 <kimjihoon6188@daum.net>
2018-11-27 22:04:07 +0900

    Add start animation

--- Assets/Scripts/GameStartManager.cs
+++ Assets/Scripts/GameStartManager.cs
@@ -2,9 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameStartManager : MonoBehaviour
 {
+    public Image background;
+    public Sprite[] startAnimation;
+
+    void Start()
+    {
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate()
+    {
+        foreach (Sprite sprite in startAnimation)
+        {
+            background.sprite = sprite;
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
     public void OnSelectButtonClick(string typeName)
     {
         PlayerType type = (PlayerType)System.Enum.Parse(typeof(PlayerType), typeName);
